096bcc789fa0b0552a052ed9c02a1fa5d0562d95
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-07-09 12:46:38 +1000

    readme - datetime

--- src/app.py
+++ src/app.py
@@ -1,2 +1,52 @@
+## GLOBAL VARIABLES
+
+def get_input(prompt):
+    user_input = input(prompt)
+    return user_input
+
+def choose_option():
+    options = {
+        "A": add_task,
+        "M": mark_as_done,
+        "D": delete_task,
+        "C": print_completed,
+        "X": exit_program
+    }
+    while True:
+        option = get_input("> ")
+        if option not in options:
+            print("Invalid option")
+        else:
+            return options[option]()
+
+######
+
+def add_task():
+    task = get_input("Enter task > ")
+    
 
 
+
+
+def mark_as_done():
+    pass
+
+def delete_task():
+    pass
+
+def print_completed():
+    pass
+
+def exit_program():
+    pass
+
+def sort_list():
+    pass
+
+def todo_home():
+    pass
+
+
+# MAIN PROGRAM LOOP
+def main_loop():
+    pass
--- src/test_lists.py
+++ src/test_lists.py
@@ -1,18 +1,23 @@
 # python3 -m unittest test_lists.py
 
 
+# pseudo: 
+
 import unittest
 from app import add_task
 
-class TestLists(unittest.TestLists):
+class TestCases(unittest.TestCases):
 
-    def test_add_task(self):
+    def test_option_executes_function(self):
+        self.assertEqual(app.choose_option)
+
+
         
     
-    def test_mark_as_done(self):
+    # def test_mark_as_done(self):
 
     
-    def test_sort(self):
+    # def test_sort(self):
 
 if __name__ == '__main__':
     unittest.main()
