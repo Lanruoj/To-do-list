d84efefec79dabb2dff6fde0cbb3a766dbf497de
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-07-09 11:07:24 +1000

    list of features readme

---
+++ src/test_lists.py
@@ -0,0 +1,18 @@
+# python3 -m unittest test_lists.py
+
+
+import unittest
+from app import add_task
+
+class TestLists(unittest.TestLists):
+
+    def test_add_task(self):
+        
+    
+    def test_mark_as_done(self):
+
+    
+    def test_sort(self):
+
+if __name__ == '__main__':
+    unittest.main()
