9ecffdf51360237a3fb62ff106cd6f42edfaf729
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-07-11 14:57:00 +1000

    add_task errors working with loop, remaining_time working as as object, sorting by remaining_time working, main_loop working

--- src/app.py
+++ src/app.py
@@ -2,15 +2,29 @@ from datetime import date, datetime, time, timedelta
 from prettytable import PrettyTable
 
 ## GLOBAL VARIABLES ##
-date_now = date.today()
-time_now = datetime.now().strftime("%H:%M:%S")
+date_today = date.today()
+
+datetime_now = datetime.now()
+
+time_now_obj = datetime.now().time()
+
+time_now_str = datetime.now().strftime("%H:%M:%S")
+
 todo_list = []
 
 ## HOME ##
 def todo_home():
     # todo_table = PrettyTable()
     # todo_table.field_names = ["Task", "Deadline", "Time remaining"]
+    sort_by_remaining_time()
+    print(f"Time: {time_now_str}")
+    if todo_list == []:
+        print("No current tasks")
+    
+    
+
     print(todo_list)
+    print("Options:\n[A] Add a task\n[M] Mark as done\n[D] Delete a task\n[C] View completed list\n[X] Exit program")
     
 
 # Gets user input, using different prompt messages
@@ -37,41 +51,58 @@ def choose_option():
 ## ADD TASK TO LIST ##
 def add_task():
     time_format = "%H:%M"
-    time_added = datetime.now().strftime(time_format)
+    time_added_str = datetime.now().strftime(time_format)
     name = get_input("Enter task\n>")
-    ### FIX
-    deadline = "0"
-    has_deadline = get_input("Does this task have a deadline? [y/n]\n>")
-    deadline_input = ""
-    if has_deadline=="y":
-        while deadline < time_added: ### use a different condition!
-            deadline_input = get_input("Enter deadline\n>")
-            ## CHECK IF deadline_input MATCHES time_format ##
-            try: 
-                deadline_obj = datetime.strptime(deadline_input, time_format)
-                deadline = datetime.strftime(deadline_obj, time_format)
-                if deadline < time_added:
-                    print("Time must be in the future")
-                else:
-                    break
+    # has_deadline = get_input("Does this task have a deadline? [y/n]\n>")
+    # deadline_input = ""
+    # if has_deadline=="y":
+        # while True: ### use a different condition!
+    ## CHECK IF deadline_input MATCHES time_format ##
+    while True:
+        deadline_input = get_input("Enter deadline\n>")
+        try: 
+            datetime_now = datetime.now()
+            deadline_obj = datetime.strptime(deadline_input, time_format).time()
+            # deadline_str = datetime.strftime(deadline_input, time_format)
+            if deadline_obj < time_now_obj:
+                print("ERROR: Time must be in the future")
+                
+            else:
+                deadline_dt = datetime.combine(datetime_now, deadline_obj)
+                time_now_dt = datetime.combine(datetime_now, time_now_obj)
+                remaining_time = deadline_dt - time_now_dt
+                task = (name, time_added_str, deadline_obj, remaining_time)
+                print(f"Remaining time: {remaining_time} minutes")
+                todo_list.append(task)
+                break
 
-            except ValueError:
-                    print("Invalid time format")
+        except ValueError:
+                print("ERROR: Invalid time format")
+    
 
+    # deadline_dt = datetime.combine(datetime_now, deadline_obj)
+    # time_now_dt = datetime.combine(datetime_now, time_now_obj)
+    # remaining_time = deadline_dt - time_now_dt
+    # task = (name, time_added_str, deadline_obj, remaining_time)
+    # print(f"Remaining time: {remaining_time} minutes")
+
+    
+    
+    # print(f"Remaining time: {remaining_time} minutes")
 
     ## CREATE TASK OBJECT AS A TUPLE OF VALUES
-    task = (name, time_added, deadline)
+    # task = (name, time_added_str, deadline_obj, remaining_time)
     ## ADD TASK TO GLOBAL LIST
-    todo_list.append(task)
+    # todo_list.append(task)
 
 
 ## EXTRACT deadline FROM task TUPLE ##
-def extract_deadline(task):
-    return task[2] 
+def extract_remaining_time(task):
+    return task[3] 
 
 ## SORT LIST BY DEADLINE TIME ##
-def sort_by_deadline():
-    todo_list.sort(key=extract_deadline)
+def sort_by_remaining_time():
+    todo_list.sort(key=extract_remaining_time)
 
 
 
@@ -88,9 +119,10 @@ def exit_program():
     pass
 
 
-# MAIN PROGRAM LOOP
+## MAIN PROGRAM LOOP
 def main_loop():
     while True:
+        sort_by_remaining_time()
         todo_home()
         choose_option()
 
