6e5a0f56cff14f3e7fd592ad39b7d98934b2b8a8
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-07-11 17:27:17 +1000

    need to deadline==n

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from prettytable import PrettyTable
 
 ## GLOBAL VARIABLES ##
 date_today = date.today()
+time_format = "%H:%M"
 
 
 
@@ -35,30 +36,30 @@ def choose_option():
 def add_task():
     datetime_now = datetime.now()
     time_now_obj = datetime.now().time()
-    time_format = "%H:%M"
     time_added_str = datetime.now().strftime(time_format)
     name = get_input("Enter task\n>")
 
-    # has_deadline = get_input("Does this task have a deadline? [y/n]\n>")
-    ## CHECK IF deadline_input MATCHES time_format ##
-    while True:
-        deadline_input = get_input("Enter deadline\n>")
-        try: 
-            datetime_now = datetime.now()
-            deadline_obj = datetime.strptime(deadline_input, time_format).time()
-            # deadline_str = datetime.strftime(deadline_input, time_format)
-            if deadline_obj < time_now_obj:
-                print("ERROR: Time must be in the future")               
-            else:
-                deadline_dt = datetime.combine(datetime_now, deadline_obj)
-                time_now_dt = datetime.combine(datetime_now, time_now_obj)
-                remaining_time = deadline_dt - time_now_dt
-                task = (name, time_added_str, deadline_obj, remaining_time)
-                print(f"Remaining time: {remaining_time} minutes")
-                todo_list.append(task)
-                break
-        except ValueError:
-                print("ERROR: Invalid time format")
+    has_deadline = get_input("Does this task have a deadline? [y/n]\n>")
+    if has_deadline == "y":
+        ## CHECK IF deadline_input MATCHES time_format ##
+        while True:
+            deadline_input = get_input("Enter deadline\n>")
+            try: 
+                datetime_now = datetime.now()
+                deadline_obj = datetime.strptime(deadline_input, time_format).time()
+                # deadline_str = datetime.strftime(deadline_input, time_format)
+                if deadline_obj < time_now_obj:
+                    print("ERROR: Time must be in the future")               
+                else:
+                    deadline_dt = datetime.combine(datetime_now, deadline_obj)
+                    time_now_dt = datetime.combine(datetime_now, time_now_obj)
+                    remaining_time = deadline_dt - time_now_dt
+                    break
+            except ValueError:
+                    print("ERROR: Invalid time format")
+
+    task = (name, time_added_str, deadline_obj, remaining_time)
+    todo_list.append(task)
 
 
 
@@ -88,7 +89,7 @@ def exit_program():
 
 ## HOME ##
 def todo_home():
-    time_now_str = datetime.now().strftime("%H:%M:%S")
+    time_now_str = datetime.now().strftime(time_format)
     sort_by_remaining_time()
     todo_table = PrettyTable()
     todo_table.field_names = ["Task", "Deadline", "Time remaining"]
