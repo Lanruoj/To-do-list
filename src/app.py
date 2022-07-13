fb9f9851791d05740b5b2568d66c21b9be2e4a33
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-07-13 16:23:48 +1000

    completed formatted, removed seconds manually - need to improve

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from datetime import date, datetime, time, timedelta
 from prettytable import PrettyTable
+import os
 
 ## GLOBAL VARIABLES ##
 time_format = "%H:%M"
@@ -9,10 +10,15 @@ completed_list = []
 # Gets user input, using different prompt messages
 def get_input(prompt):
     user_input = input(prompt)
-    if user_input == "X":
+    return user_input
+
+def exit_program():
+    confirmation = get_input("Are you sure you want to quit? [y/n]\n> ")
+    if confirmation == "y":
+        print("Have a great day!")
         quit()
     else:
-        return user_input
+        return
 
 
 # User chooses what they'd like the program to do via user input
@@ -22,7 +28,7 @@ def choose_option():
         "M": mark_as_done,
         "D": delete_task,
         "C": print_completed,
-        # "X": exit_program
+        "X": exit_program
     }
     while True:
         option = get_input("> ")
@@ -35,6 +41,8 @@ def choose_option():
 def mark_as_done():
     while True:
         task = get_input("Enter completed task ([back] to cancel)\n> ")
+        if task == "back":
+            return 
         for i in todo_list:
             if task == i[0]:
                 time_added_str = datetime.now().strftime(time_format)
@@ -43,12 +51,7 @@ def mark_as_done():
                 todo_list.remove(i)
                 return completed_list
         else:
-            if task == "back":
-                return 
-            else:
-                print("ERROR: No existing task")
-                
-    
+            print("ERROR: No existing task")  
 
 
 def delete_task():
@@ -63,13 +66,22 @@ def delete_task():
 
 
 def print_completed():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    dt_now = datetime.now()
+    day_name = dt_now.strftime("%A")
+    date_today = date.today()
+    time_now_str = datetime.now().strftime(time_format)
     completed_table = PrettyTable()
     completed_table.field_names = ["Task", "Time completed"]
 
-    for task in completed_list:       
-        completed_table.add_row([task[0], task[1]])
-    
-    print(completed_table)
+    if len(completed_list) >= 1:
+        for task in completed_list:       
+            completed_table.add_row([task[0], task[1]])
+    else:
+        completed_table.add_row(["No completed tasks", "-"])  
+        
+    display = f"\n----TO-DO LIST APP----\nDate: {day_name}, {date_today}\nTime: {time_now_str}\n\n----COMPLETED TASKS----\n{completed_table}\n"
+    print(display)
     option = get_input("Enter [y] to continue\n> ")
     if option == "y":
         todo_home()
@@ -77,6 +89,7 @@ def print_completed():
 
 ## HOME ##
 def todo_home():
+    os.system('cls' if os.name == 'nt' else 'clear')
     dt_now = datetime.now()
     day_name = dt_now.strftime("%A")
     sort_by_rem_time()
@@ -97,7 +110,7 @@ def todo_home():
             else:
                 todo_table.add_row([name, deadline, rem_time])
 
-    home_display = f"\n----TO-DO LIST APP----\nDate: {day_name}, {date_today}\nTime: {time_now_str}\n{todo_table}\nOptions:\n[A] Add a task\n[M] Mark as done\n[D] Delete a task\n[C] View completed list\n[X] Exit program"
+    home_display = f"\n----TO-DO LIST APP----\nDate: {day_name}, {date_today}\nTime: {time_now_str}\n\n----REMAINING TASKS----\n{todo_table}\nOptions:\n[A] Add a task\n[M] Mark as done\n[D] Delete a task\n[C] View completed list\n[X] Exit program"
     return home_display
     
 
@@ -138,11 +151,16 @@ def calculate_rem_time(deadline):
         time_now_obj = datetime.now().time()
         deadline_dt = datetime.combine(datetime_now, deadline)
         time_now_dt = datetime.combine(datetime_now, time_now_obj)
-        rem_time = deadline_dt - time_now_dt
+        td = deadline_dt - time_now_dt
+        seconds = td.seconds
+        rem_time = timedelta(seconds = seconds)
+        rem_time_str = str(rem_time)
+        return rem_time_str[:-3]
     else:
-        rem_time = None
+        rem_time_str = None
+        return rem_time_str
 
-    return rem_time
+
 
 def sort_by_rem_time():
     todo_list.sort(key=lambda x: (x[1] is None, x[1]))
